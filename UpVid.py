118e0274b0a0040ad0dddcf647e7ab89f540fe3d
Frael4 <frael.o1805@gmail.com>
2023-04-27 10:47:01 -0500

    correcciones

--- UpVid.py
+++ UpVid.py
@@ -1,98 +1,268 @@
 import os
+import shutil
 from datetime import datetime
 #
 import google.auth
 import google.auth.transport.requests
+from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build # Proporciona la interfaz de interaccion con los servicios de google
+# Proporciona la interfaz de interaccion con los servicios de google
+from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 class UpVid:
-        
-    EXT_VID = ['.mp4']
-    DIRECTORIO = '' #os.getcwd()
 
-    # Cuenta
-    USUARIO = ''
-    PASS = ''
+    EXT_VID = ['.mp4']
+    # configuraciones
+    SETTINGS = {}
+
+    # Credenciales
     CREDS = None
-    
-    #
-    FILE_TO_UPLOAD = ''
-    VIDEOS_PROP = []
+    CREDS2 = None
+
+    # nombre.mp4
+    VID_TO_UPLOAD = []
+
+    # propiedades del video: ruta, nombre
+    VIDEO_PROP = []
 
     def __init__(cls):
         print(cls)
 
-    def setAccount(cls):
-        cls.USUARIO = ''
-        cls.PASS = ''
+    @classmethod
+    def setSettings(cls, settings):
+        """ Setea directorio de videos """
+        if cls.SETTINGS != settings:
+            print({settings})
+            cls.SETTINGS['DIRECTORIO'] = settings['DIRECTORIO']
+            cls.SETTINGS['vidsToUpload'] = settings['vidsToUpload']
 
-    def setDir(cls, directorio):
-        if cls.DIRECTORIO == '':
-            print({directorio})
-            cls.DIRECTORIO = directorio
-
+    @classmethod
     def autenticar(cls):
-        if os.path.exists('token.json') :
+        """ Autenticacion de la aplicacion y el usuario """
+        if os.path.exists('token.json') and os.path.exists('token_r.json'):
             cls.CREDS = Credentials.from_authorized_user_file('token.json')
+            cls.CREDS2 = Credentials.from_authorized_user_file('token_r.json')
         if not cls.CREDS or not cls.CREDS.valid:
-            if cls.CREDS and cls.CREDS.expired and cls.CREDS.refresh_token:
-                cls.CREDS.refresh(google.auth.transport.requests.Request()) #
-            else :
+            if (cls.CREDS or cls.CREDS2) and (cls.CREDS.expired or cls.CREDS2.expired) and (cls.CREDS.refresh_token or cls.CREDS2.refresh_token):
+                cls.CREDS.refresh(google.auth.transport.requests.Request())
+                cls.CREDS2.refresh(google.auth.transport.requests.Request())
+            else:
                 flujo = InstalledAppFlow.from_client_secrets_file(
-                    'client_secret.json', scopes = ['https://www.googleapis.com/auth/youtube.upload']
+                    'client_secret.json', scopes=['https://www.googleapis.com/auth/youtube.upload']
                 )
                 cls.CREDS = flujo.run_local_server(port=0)
+
+                flujo = InstalledAppFlow.from_client_secrets_file(
+                    'client_secret.json', scopes=['https://www.googleapis.com/auth/youtube.readonly']
+                )
+                cls.CREDS2 = flujo.run_local_server(port=0)
             with open('token.json', 'w') as token:
                 token.write(cls.CREDS.to_json())
-            
-    def uploadVid(cls):
-        youtube = build('youtube', 'v3', credentials=cls.CREDS)
-        video_to_up = MediaFileUpload(cls.FILE_TO_UPLOAD, chunksize=-1, resumable=True)
+            with open('token_r.json', 'w') as token:
+                token.write(cls.CREDS2.to_json())
 
-        request = youtube.videos().insert()
-
+    @classmethod
     def searchVids(cls):
-        os.chdir(cls.DIRECTORIO) # setea el directorio
+        """ Busca los videos en el directorio """
+        os.chdir(cls.SETTINGS['DIRECTORIO'])  # setea el directorio
         files = os.listdir()
-        """ print(f'Archivos del dir') """
-        print(files)
         vids = []
         for f in files:
             name, _ext = os.path.splitext(f)
             if _ext in cls.EXT_VID:
                 vids.append(f)
         if vids == []:
-            print('No videos to upload!')
-        else:
-            cls.searchCurrenVid(vids)
-                
+            print('No videos en directorio!')
+        if cls.SETTINGS['vidsToUpload'] == 1:
+            cls.VID_TO_UPLOAD.append(cls.searchCurrentVid(vids))
+        elif cls.SETTINGS['vidsToUpload'] == 2:
+            cls.VID_TO_UPLOAD = vids
 
-    def searchCurrenVid(cls, vids):
+    @classmethod
+    def searchCurrentVid(cls, vids):
+        """ Busca el video actual para subir """
         for v in vids:
             prop = os.stat(v)
-            #print(f'Fecha del vid {datetime.fromtimestamp(prop.st_ctime).strftime("%d/%m/%y")}')
-            fechaVid = datetime.fromtimestamp(prop.st_ctime).strftime("%d/%m/%Y")
+            fechaVid = datetime.fromtimestamp(
+                prop.st_ctime).strftime("%d/%m/%Y")
             actualFecha = datetime.now().strftime("%d/%m/%y")
-            #print(fechaVid)
             if fechaVid == actualFecha:
-                cls.FILE_TO_UPLOAD = v
-                #cls.VIDEOS_PROP = os.stat(v)
-                print(cls.FILE_TO_UPLOAD)
+                print(f'Video actual es : {v}')
+                return v
 
+    @classmethod
     def prepareVid(cls):
-        if cls.FILE_TO_UPLOAD == '':
+        """ Setea lass propiedades del video """
+        if cls.VID_TO_UPLOAD == [] or cls.VID_TO_UPLOAD == '':
             print('Not vid to upload')
-            return
+            return False
+        for _v_to_up in (cls.VID_TO_UPLOAD):
+            ruta_video = os.path.join(
+                cls.SETTINGS['DIRECTORIO'], _v_to_up)
+            titulo_video = os.path.basename(
+                os.path.join(cls.SETTINGS['DIRECTORIO'], _v_to_up))
 
-        ruta_video = os.path.join(os.getcwd(), cls.FILE_TO_UPLOAD)
-        titulo_video = os.path.basename(os.path.join(os.getcwd, cls.FILE_TO_UPLOAD))
+            video = VideoFileClip(ruta_video)
+            duracion = int(video.duration)
+            horas = (duracion // 3600)
+            minutos = (duracion // 60)
+            segundos = (duracion % 60)
+            print(
+                f'Duracion del video es {horas} horas con {minutos} minutos y {segundos} segundos')
 
+            video_prop = {
+                'ruta_video': ruta_video,
+                'titulo_video': titulo_video,
+                'duracion': duracion,
+                'duracion_horas': horas,
+                'duracion_minutos': minutos,
+                'duracion_segundos': segundos
+            }
+
+            cls.VIDEO_PROP.append(video_prop)
+
+            if horas >= 24:
+                if cls.dividirVideo(video=video, video_prop=video_prop):
+                    cls.uploadVid()
+            else:
+                cls.uploadVid()
+
+
+    def verificarVideoUploaded(cls, video_title):
+        youtube = build('youtube', 'v3', credentials=cls.CREDS2)
+        # Hacemos una solicitud de búsqueda en la lista de reproducción de nuestro canal de YouTube
+        request = youtube.videos().list(
+            part="snippet",
+            id='UCV_g0WY6argVD0LXTFa1n6w',
+            # mine=True,
+            # playlistId=PLAYLIST_ID,
+            maxResults=50
+        )
+        response = None
+        try:
+            response = request.execute()
+        except HttpError as e:
+            print(f'Error en la peticion {e}')
+            return False
+
+        if response['items'] == []:
+            print(response)
+            print(f'No se encontraron items')
+            return False
+        else:
+            # Verificamos si el título del video ya está en la lista de reproducción
+            for item in response['items']:
+                if item['snippet']['title'] == video_title:
+                    print("El video ya ha sido subido.")
+                    return True
+
+    @classmethod
+    def uploadVid(cls):
+        """ Subida del archivo a la cuenta """
+        # print(cls.verificarVideoUploaded(cls, video_title=cls.VID_TO_UPLOAD[0]['titulo_video']))
+        for vids in cls.VIDEO_PROP:
+            if cls.verificarVideoUploaded(cls, video_title=vids['titulo_video']):
+                print(f'Preparando peticion ')
+                youtube = build('youtube', 'v3', credentials=cls.CREDS)
+                video_to_up = MediaFileUpload(
+                    vids['ruta_video'], chunksize=-1, resumable=True)
+                video_to_up_desc = {
+                    'snippet': {
+                        'title': vids['titulo_video'],
+                        'description': 'video ',
+                        'tags': [],
+                        'categoryId': ''
+                    },
+                    'status': {
+                        'privacyStatus': 'private'
+                    }
+                }
+                response = None
+                try:
+                    request = youtube.videos().insert(
+                        part='snippet,status',
+                        body=video_to_up_desc,
+                        media_body=video_to_up
+                    )
+
+                    while response is None:
+                        estado, response = request.next_chunk()
+                        if estado:
+                            print(
+                                'Subiendo video ', vids['titulo_video'], f': {int(estado.progress() *100)}% completado')
+
+                    print('el video ', vids['titulo_video'],
+                          ' se ha subido con exito')
+                except HttpError as e:
+                    print('Error en subida de video ',
+                          vids['titulo_video'], f' - {e}')
+
+                cls.moveVidUploaded(ruta=vids['ruta_video'])
+            else:
+                print('video ya subido')
+                cls.moveVidUploaded(ruta=vids['ruta_video'])
+
+    @classmethod
+    def moveVidUploaded(cls, ruta):
+        """ Mover video subido """
+        print('Moviendo video a subidos ...')
+        if os.path.exists(os.path.join(cls.SETTINGS['DIRECTORIO'],'upvid - vidsUploaded')):
+            shutil.move(ruta, os.path.join(
+                cls.SETTINGS['DIRECTORIO'], 'upvid - vidsUploaded'))
+        else:
+            os.mkdir('upvid - vidsUploaded')
+            cls.moveVidUploaded(ruta=ruta)
+
+    @classmethod
+    def dividirVideo(cls, video, video_prop):
+        """ Divide el video en dos partes si este es de una duracion mayor o igual a 12 horas """
+        parte_1 = None
+        parte_2 = None
+
+        mitad = video_prop['duracion'] // 2
+        parte_1 = video.subclip(0, mitad)
+        parte_2 = video.subclip(mitad)
+
+        titulo_1 = video_prop['titulo_video'] + '_1'
+        titulo_2 = video_prop['titulo_video'] + '_2'
+        ruta_1 = os.path.join(cls.SETTINGS['DIRECTORIO'], titulo_1 + '.mp4')
+        ruta_2 = os.path.join(cls.SETTINGS['DIRECTORIO'], titulo_2 + '.mp4')
+        try:
+            print(f'Dividiendo videos ... ')
+            parte_1.write_videofile(ruta_1)
+            parte_2.write_videofile(ruta_2)
+
+            subvid_1 = {
+                'ruta_video': ruta_1,
+                'titulo_video': titulo_1
+            }
+            subvid_2 = {
+                'ruta_video': ruta_2,
+                'titulo_video': titulo_2
+            }
+            cls.VIDEO_PROP.append(subvid_1)
+            cls.VIDEO_PROP.append(subvid_2)
+        except Exception as er:
+            print(f'Ocurrio un error en la division del video: {er}')
+            return False
+        return True
+
+    def deleteVideo(cls):
+        pass
+
+    @classmethod
     def main(cls):
-        print()
-        #upload():
+        """ Metodo principal """
+        print('Autenticando ...')
+        # upload():
+        cls.autenticar()
+        print('Buscando Video ...')
         cls.searchVids()
+        print('Preparando video ...')
+        cls.prepareVid()
+            
--- app.py
+++ app.py
@@ -1,42 +1,155 @@
-import tkinter as tk #Libreria grafica
+import os
+import json
+import tkinter as tk  # Libreria grafica
 from tkinter import filedialog
 from UpVid import UpVid
 
-def openFile(input):
-    ruta = filedialog.askdirectory(initialdir = '/', title= 'Seleccione una carpeta')
-    input.insert(0, ruta)
-    #print(ruta)
 
-def saveSettings():
-    #botonSaveAll.config(state='disabled') desactivamos el boton
-    ob = UpVid()
-    ruta = input.get()
-    print(f'Esta es la ruta {ruta}')
-    ob.setDir(ruta)
-    ob.main()
+class App:
 
-app = tk.Tk() # instanciacion
-app.geometry('500x200') # Configuracion dimensiones
-app.configure()
+    APP_PATH = os.getcwd()
+    # Archivo de configuracion
+    CONFG = {
+        'DIRECTORIO': '/',
+        'vidsToUpload': 1,
+        'periodoBackup': 0
+    }
 
-tk.Wm.wm_title(app, "Vid Uploads - Youtube")
+    def __init__(cls):
+        cls.app = tk.Tk()  # instanciacion
+        cls.frameOne = tk.Frame(cls.app, borderwidth=1, relief='groove', width=380, height=200)
+        cls.etiquetaUser = tk.Label(cls.app, text='User!')
 
-etiquetaUser = tk.Label(app)
+        cls.etiquetaRuta = tk.Label(
+            cls.frameOne, text='Seleccione ruta de respaldo', font=('Times New Roman', 10))
 
-etiquetaUser.pack()
-etiquetaRuta = tk.Label(app, 
-                        text='Seleccione ruta de respaldo',
-                        font=('Helvetica',14))
-""" etiquetaRuta.grid(row=0, column=1) """
-etiquetaRuta.pack(padx = 2, pady=2)
+        cls.varStringInputDir = tk.StringVar()
+        cls.input = tk.Entry(cls.frameOne)
+        cls.botonOpenDir = tk.Button(cls.frameOne, text='Escoger carpeta',
+                                     command=lambda: cls.openFile(cls.input))
+        
+        cls.etiquetaRadioButtons = tk.Label(
+            cls.frameOne, text='Metodo de subida', font=('Times New Roman', 10))
+        cls.radioGroupValue = tk.IntVar()
+        cls.radioButton = tk.Radiobutton(cls.frameOne)
+        cls.radioButton1 = tk.Radiobutton(cls.frameOne)
+        
+        cls.etiquetaPeriodoRespaldo = tk.Label(
+            cls.frameOne, text='Respaldar cada:', font=('Times New Roman', 10))
+        cls.inputPeriodo = tk.Entry(cls.frameOne)
+        cls.etiquetaPeriodoRespaldo_ = tk.Label(
+            cls.frameOne, text=' horas', font=('Times New Roman', 10))
 
-input = tk.Entry(app)
-input.pack()
+        cls.botonSaveAll = tk.Button(
+            cls.app, text='Guardar configuración', command=cls.saveSettings)
+        cls.botonEdit = tk.Button(
+            cls.app, text='Editar configuración', command=cls.editSettings)
+        
+        cls.botonStart = tk.Button(text='Start!', command=cls.startUpload)
 
-boton = tk.Button(app, text='Escoger carpeta', command= lambda: openFile(input))
-boton.pack()
+    def getSettings(cls):
+        if not os.path.exists('confg.json'):
+            with open(os.path.join(cls.APP_PATH, 'confg.json'), 'w') as confg:
+                json.dump(cls.CONFG, confg)
+        else:
+            with open(os.path.join(cls.APP_PATH, 'confg.json'), 'r') as confg:
+                cls.CONFG = json.load(confg)
+                print('aqui entro')
 
-botonSaveAll = tk.Button(app, text='Guardar!', command= saveSettings)
-botonSaveAll.pack()
+    def openFile(cls, input):
+        input.delete(0, tk.END)
+        ruta = filedialog.askdirectory(
+            initialdir='/', title='Seleccione una carpeta')
+        cls.varStringInputDir.set(ruta)
+        # print(ruta)
 
-app.mainloop()# Ejecucion de la ventana
+    def saveSettings(cls):
+        ruta = cls.input.get()
+        valorRadio = cls.radioGroupValue.get()
+        print(f'Esta es la ruta {ruta}')
+        if ruta == '':
+            print('ruta no puede ser vacia')
+            return
+        with open(os.path.join(cls.APP_PATH, 'confg.json'), 'r') as confg:
+            cls.CONFG = json.load(confg)
+            cls.CONFG['DIRECTORIO'] = ruta
+            cls.CONFG['vidsToUpload'] = valorRadio
+        with open(os.path.join(cls.APP_PATH, 'confg.json'), 'w') as confg:
+            json.dump(cls.CONFG, confg)
+        # deshabilitamos botones
+        cls.radioButton.config(state='disabled')
+        cls.radioButton1.config(state='disabled')
+        cls.botonOpenDir.config(state='disabled')
+        cls.botonSaveAll.config(state='disabled')
+        cls.inputPeriodo.config(state='disabled')
+        cls.botonEdit.config(state='normal')
+        cls.botonStart.config(state='normal')
+        
+        # ob.main()
+
+    def editSettings(cls):
+        cls.botonEdit.config(state='disabled')
+        # habilitar botones
+        cls.botonOpenDir.config(state='active')
+        cls.radioButton.config(state='normal')
+        cls.radioButton1.config(state='normal')
+        cls.botonOpenDir.config(state='normal')
+        cls.botonSaveAll.config(state='normal')
+        cls.inputPeriodo.config(state='normal')
+
+
+    def startUpload(cls):
+        ob = UpVid()
+        ob.setSettings(cls.CONFG['DIRECTORIO'])
+        ob.main()
+
+
+    def setInstances(cls):
+        cls.app.geometry('400x350')  # Configuracion dimensiones
+        cls.app.configure()
+        tk.Wm.wm_title(cls.app, "Vid Uploads - Youtube")
+        #cls.frameOne.configure(width=480,height=100)
+        cls.etiquetaUser.pack()
+        cls.frameOne.pack()
+
+        cls.etiquetaRuta.pack(padx=2, pady=2)
+
+        ruta_default = cls.CONFG['DIRECTORIO'] if len(
+            cls.CONFG['DIRECTORIO']) > 0 else '/'
+        cls.varStringInputDir.set(ruta_default)
+        cls.input.config(state='disabled', textvariable=cls.varStringInputDir)
+        cls.input.configure(width=50)
+        cls.input.pack()
+        cls.botonOpenDir.config(state='disabled')
+        cls.botonOpenDir.pack()
+
+        cls.etiquetaRadioButtons.pack()
+        vidsToUpload = cls.CONFG['vidsToUpload']
+        cls.radioGroupValue.set(vidsToUpload)
+        cls.radioButton.config(state='disabled',text='Vids actuales', variable=cls.radioGroupValue, value=1)
+        cls.radioButton1.config(state='disabled',text='Todos', variable=cls.radioGroupValue, value=2)
+        cls.radioButton.pack()
+        cls.radioButton1.pack()
+
+        cls.etiquetaPeriodoRespaldo.pack()
+        cls.inputPeriodo.config(state='disabled')
+        cls.inputPeriodo.configure(width=5)
+        cls.inputPeriodo.pack()
+        cls.etiquetaPeriodoRespaldo_.pack()
+
+        cls.botonSaveAll.config(state='disabled')
+        cls.botonSaveAll.pack()
+
+        cls.botonEdit.pack()
+
+        cls.botonStart.pack()
+
+    def main(cls):
+        cls.getSettings()
+        cls.setInstances()
+        cls.app.mainloop()
+
+
+app = App()
+app.main()
+# Ejecucion de la ventana
